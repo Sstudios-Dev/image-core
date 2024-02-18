a624a6c81c8205e0dc6eaac97e0021096b72197f
MasterpaintSu <159675013+MasterpaintSu@users.noreply.github.com>
2024-02-18 15:02:42 -0600

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,34 +1,29 @@
+// index.mjs
 import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
 
-const imageUrl = 'https://raw.githubusercontent.com/Sstudios-Dev/image-core/main/src/img/'; // Ruta base del repositorio
-
-// Genera un número aleatorio entre 1 y 100
-const randomNumber = Math.floor(Math.random() * 9) + 1;
-
-// Nombre de la imagen
-const imageName = `img-core${randomNumber}.jpg`;
-
-// URL completa de la imagen
-const imageUrlFull = `${imageUrl}${imageName}?raw=true`;
-
-// Directorio donde se guardará la imagen
-const directory = './images';
-
-// Crea el directorio si no existe
-if (!fs.existsSync(directory)) {
-  fs.mkdirSync(directory);
+async function getImageUrlFull() {
+  const imageUrl = 'https://raw.githubusercontent.com/Sstudios-Dev/image-core/main/src/img/';
+  const randomNumber = Math.floor(Math.random() * 24) + 1;
+  const imageName = `img-core${randomNumber}.jpg`;
+  return { imageUrlFull: `${imageUrl}${imageName}?raw=true`, imageName };
 }
 
-// Descarga la imagen
-(async () => {
+async function downloadRandomImage() {
+  const { imageUrlFull, imageName } = await getImageUrlFull();
+  const directory = './images';
+
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+
   const response = await fetch(imageUrlFull);
   const buffer = await response.buffer();
+  
   fs.writeFileSync(path.join(directory, imageName), buffer);
   console.log(`Imagen ${imageName} descargada`);
+}
 
-  console.log(`Link directo de la imagen: ${imageUrlFull}`);
-
-  // Aquí puedes hacer algo con la imagen descargada, como mostrarla en un navegador o en una aplicación de escritorio
-})();
+// Exportar las funciones y imageName
+export { getImageUrlFull, downloadRandomImage };
