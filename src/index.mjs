8ce63ed258980c8b27ccdbbb2616ad24afaaf517
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-02-17 21:09:50 -0600

    Update and rename index.js to index.mjs

---
+++ src/index.mjs
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+
+const imageUrl = 'https://raw.githubusercontent.com/Sstudios-Dev/image-core/main/src/img/'; // Ruta base del repositorio
+
+// Genera un número aleatorio entre 1 y 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Nombre de la imagen
+const imageName = `img-core${randomNumber}.jpg`;
+
+// URL completa de la imagen
+const imageUrlFull = `${imageUrl}${imageName}`;
+
+// Directorio donde se guardará la imagen
+const directory = './images';
+
+// Crea el directorio si no existe
+if (!fs.existsSync(directory)) {
+  fs.mkdirSync(directory);
+}
+
+// Descarga la imagen
+(async () => {
+  const response = await fetch(imageUrlFull);
+  const buffer = await response.buffer();
+  fs.writeFileSync(path.join(directory, imageName), buffer);
+  console.log(`Imagen ${imageName} descargada`);
+
+  // Aquí puedes hacer algo con la imagen descargada, como mostrarla en un navegador o en una aplicación de escritorio
+})();
