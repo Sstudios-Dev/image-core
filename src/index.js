ec667b1fbd1dca631eb90c171b0ec471d72e073b
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-03-02 20:21:12 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,4 +26,32 @@ async function downloadRandomImage() {
   }
 }
 
-module.exports = { getImageUrlFull, downloadRandomImage };
+//////////////////////////////////////END MODULE 1 //////////////////////////////////////
+
+async function getRandomAnimeImageUrl() {
+  const animeImageUrl = 'https://raw.githubusercontent.com/Sstudios-Dev/image-core/main/src/img-anime/';
+  const randomNumber = Math.floor(Math.random() * 200) + 1; // Assuming there are 1000 images
+  const imageName = `img-anime${randomNumber}.jpg`;
+  return { animeImageUrlFull: `${animeImageUrl}${imageName}?raw=true`, imageName };
+}
+
+async function downloadRandomAnimeImage() {
+  const { animeImageUrlFull, imageName } = await getRandomAnimeImageUrl();
+  const directory = './anime-images';
+
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+
+  try {
+    const response = await axios.get(animeImageUrlFull, { responseType: 'arraybuffer' });
+    fs.writeFileSync(path.join(directory, imageName), Buffer.from(response.data));
+    console.log(`Anime image ${imageName} downloaded`);
+  } catch (error) {
+    console.error(`Failed to download image ${imageName}: ${error.message}`);
+  }
+}
+
+//////////////////////////////////////END MODULE 2 //////////////////////////////////////
+
+module.exports = { getImageUrlFull, downloadRandomImage, getRandomAnimeImageUrl, downloadRandomAnimeImage };
